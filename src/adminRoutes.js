4e1de756ef4ec9f8168b012b65d1c89321a5c202
JALAGANDESWER.R <jalagandeswer.r@gmail.com>
2022-01-08 01:45:15 +0530

    admin

--- src/adminRoutes.js
+++ src/adminRoutes.js
@@ -107,7 +107,7 @@ app.get("/users/:id",async (req,res)=>{
         if(!user)throw new Error("invalid user id");
         user=await user.populate({
             path:"payments",
-            select:"-id -updatedAt",
+            select:"-updatedAt",
             options:{
                 sort:{
                     createdAt:-1
