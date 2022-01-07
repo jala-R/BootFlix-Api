22f523f06c12831ac814c71aa548b4a8b7331e46
JALAGANDESWER.R <jalagandeswer.r@gmail.com>
2022-01-08 01:43:24 +0530

    admin

--- src/adminRoutes.js
+++ src/adminRoutes.js
@@ -114,6 +114,7 @@ app.get("/users/:id",async (req,res)=>{
                 }
             }
         })
+        console.log(user)
         res.send(user);
     }catch(err){
         res.status(400).send(err.message);
