55c3a719ee29cf71412797305478cc0d9538a5b2
JALAGANDESWER.R <jalagandeswer.r@gmail.com>
2022-01-07 20:15:57 +0530

    payment

--- src/userRoutes.js
+++ src/userRoutes.js
@@ -200,14 +200,14 @@ app.get("/getPaymentList",loginMiddleware,async (req,res)=>{
     // console.log(req.user.payments);
     await (req.user.populate({
         path:"payments",
-        select:"toPlan createdAt",
+        select:"toPlan createdAt -userId",
         options:{
             sort:{
                 createdAt:-1
             }
         }
     }));
-    // console.log(req.user.payments);
+    console.log(req.user.payments);
     res.send(req.user.payments)
 })
 
