09b7dc5803744c49f387409488b27adb2d7f8fc0
JALAGANDESWER.R <jalagandeswer.r@gmail.com>
2022-01-15 14:20:48 +0530

    admin

--- helper/getAccountAge.js
+++ helper/getAccountAge.js
@@ -4,12 +4,7 @@ function getAccountAge(curMonth,curYear,dateOfCreation){
     let yearDiff=curYear-docYear;
     let monthDiff=curMonth-docMonth;
     let diff=0;
-    if(monthDiff>=0){
-        diff=monthDiff+yearDiff*12;
-    }else{
-        yearDiff--;
-        diff=Math.abs(monthDiff)+yearDiff*12;
-    }
+    diff=monthDiff+yearDiff*12;
     return diff;
 }
 
