344640cf4ba1335bc9f6b5f2c714113c13accdc6
JALAGANDESWER.R <jalagandeswer.r@gmail.com>
2022-01-21 14:50:51 +0530

    bug

--- src/userRoutes.js
+++ src/userRoutes.js
@@ -80,13 +80,13 @@ app.get("/logout",loginMiddleware,async (req,res)=>{
 })
 
 
-app.get("/movie/:movieId",(req,res)=>{
-    // console.log(req.params)
-    res.send(`<video width="320" height="240" controls>
-    <source src="/movie/${req.params.movieId}/watch" type="video/mp4">
-    Your browser does not support the video tag.
-  </video>`)
-})
+// app.get("/movie/:movieId",(req,res)=>{
+//     // console.log(req.params)
+//     res.send(`<video width="320" height="240" controls>
+//     <source src="/movie/${req.params.movieId}/watch" type="video/mp4">
+//     Your browser does not support the video tag.
+//   </video>`)
+// })
 
 app.get("/movie/:movieId/watch",async (req,res)=>{
     let start=Number(req.headers.range.slice(6,req.headers.range.length-1));
