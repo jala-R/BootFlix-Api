29ad8d9d6783ec3942b4e7cb6059fcbd67c7905b
JALAGANDESWER.R <jalagandeswer.r@gmail.com>
2022-01-22 16:02:11 +0530

    bug

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use((req,res,next)=>{
     res.set('Access-Control-Allow-Origin',`${req.headers.origin}`)
     res.set("Access-Control-Allow-Headers","Content-Type");
     res.set("Access-Control-Allow-Credentials","true");
-    res.set("Access-Control-Allow-Methods","*");
+    res.set("Access-Control-Allow-Methods","GET,POST,DELETE,OPTIONS","PUT");
     next();
 })
 
