daa0abf7649c4a73b8d2d250c36235f5891381f8
JALAGANDESWER.R <jalagandeswer.r@gmail.com>
2022-01-26 11:55:22 +0530

    final commit

--- helper/checkMovie.js
+++ helper/checkMovie.js
@@ -4,10 +4,16 @@ function checkMovie(req,res,next){
     if(!req.cookies.mid)return res.status(404).send("movie not eligible");
     let payload=jwt.verify(req.cookies.mid,process.env.JWTSECRET);
     // console.log(payload);
-    let moviePlan=payload.movie.plan;
-    let {plan:userPlan}=req.user.getPlan();
-    console.log(moviePlan,userPlan);
-    next();
+    let movieLvl=payload.movie.plan;
+    let {plan:userLvl}=req.user.getPlan();
+    if(userLvl==="Preminum")return next();
+    else if(userLvl==="Standard"){
+        if(movieLvl==="Preminum")return res.status(404).send("movie not eligible");
+        return next();
+    }else{
+        if(movieLvl!="Free")return res.status(404).send("movie not eligible");
+        return next();
+    }
 }
 
 module.exports=checkMovie;
