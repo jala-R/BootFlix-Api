4cf5c52d8e1958f57482f0d79fd38d5bf30a6162
JALAGANDESWER.R <jalagandeswer.r@gmail.com>
2022-01-26 11:35:28 +0530

    history check

--- helper/checkMovie.js
+++ helper/checkMovie.js
@@ -3,8 +3,9 @@ const jwt=require("jsonwebtoken");
 function checkMovie(req,res,next){
     if(!req.cookies.mid)res.status(404).send("movie not eligible");
     let payload=jwt.verify(req.cookies.mid,process.env.JWTSECRET);
-    console.log(req.user);
-    console.log(payload);
+    let moviePlan=paylod.plan;
+    let {plan:userPlan}=req.userPlan.getPlan();
+    console.log(moviePlan,userPlan);
     next();
 }
 
