ca97ba87c2e03409929ba21ed2512e5ee530be59
JALAGANDESWER.R <jalagandeswer.r@gmail.com>
2022-01-26 12:08:27 +0530

    final commit

--- helper/checkMovie.js
+++ helper/checkMovie.js
@@ -3,16 +3,16 @@ const jwt=require("jsonwebtoken");
 function checkMovie(req,res,next){
     if(!req.cookies.mid)return res.status(404).send("movie not eligible");
     let payload=jwt.verify(req.cookies.mid,process.env.JWTSECRET);
-    // console.log(payload);
+    console.log(payload.movie);
     let movieLvl=payload.movie.plan;
     let {plan:userLvl}=req.user.getPlan();
-    console.log(userLvl);
+    // console.log(userLvl);
     if(userLvl==="Preminum")return next();
     else if(userLvl==="Standard"){
         if(movieLvl==="Preminum")return res.status(404).send("movie not eligible");
         return next();
     }else{
-        console.log(movieLvl);
+        // console.log(movieLvl);
         if(movieLvl!="Free")return res.status(404).send("movie not eligible");
         return next();
     }
