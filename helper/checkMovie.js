4a4c8c190676b032fbdc36c2714cb8633f6b9458
JALAGANDESWER.R <jalagandeswer.r@gmail.com>
2022-01-26 11:41:10 +0530

    final bug

--- helper/checkMovie.js
+++ helper/checkMovie.js
@@ -1,9 +1,9 @@
 const jwt=require("jsonwebtoken");
 
 function checkMovie(req,res,next){
-    if(!req.cookies.mid)res.status(404).send("movie not eligible");
+    if(!req.cookies.mid)return res.status(404).send("movie not eligible");
     let payload=jwt.verify(req.cookies.mid,process.env.JWTSECRET);
-    let moviePlan=paylod.plan;
+    let moviePlan=payload.plan;
     let {plan:userPlan}=req.userPlan.getPlan();
     console.log(moviePlan,userPlan);
     next();
