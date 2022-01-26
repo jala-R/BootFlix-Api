dbab5496e0d058248f5725b01183c312a1fd22f8
JALAGANDESWER.R <jalagandeswer.r@gmail.com>
2022-01-26 11:42:06 +0530

    final bug

--- helper/checkMovie.js
+++ helper/checkMovie.js
@@ -4,7 +4,7 @@ function checkMovie(req,res,next){
     if(!req.cookies.mid)return res.status(404).send("movie not eligible");
     let payload=jwt.verify(req.cookies.mid,process.env.JWTSECRET);
     let moviePlan=payload.plan;
-    let {plan:userPlan}=req.userPlan.getPlan();
+    let {plan:userPlan}=req.user.getPlan();
     console.log(moviePlan,userPlan);
     next();
 }
